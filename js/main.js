1fc0aa0291a289ffaf9f3164f797aba38d8a068d
Massimiliano Marcon <massimiliano.marcon@nokia.com>
2012-09-12 09:59:51 +0200

    Added random tiles generation.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 (function(doc){
     'use strict';
 
-    var init, locationFound, locationNotFound,
+    Array.prototype.randomElement = Array.prototype.randomElement || function(){
+        return this[Math.floor(Math.random()*this.length)];
+    };
+
+    var init, locationFound, locationNotFound, cities,
         API = {
             id: 'C2Cse_31xwvbccaAIAaP',
             token: 'fjFdGyRjstwqr9iJxLwQ-g',
-            baseURL: 'http://m.nok.it/?app_id={ID}&token={TOKEN}&c={LAT},{LON}&nord&nodot&t=1&h=300&w=200'
+            baseURL: 'http://m.nok.it/?app_id={ID}&token={TOKEN}&c={LAT},{LON}&nord&nodot&t=1&h=200&w=200'
         };
 
     init = function(){
@@ -22,16 +26,43 @@
         var lat = position.coords.latitude,
             lon = position.coords.longitude,
             img = [],
-            url = API.baseURL.replace('{ID}', API.id).replace('{TOKEN}', API.token);
+            url = API.baseURL.replace('{ID}', API.id).replace('{TOKEN}', API.token),
+            req;
 
         //Get map tiles
         img.push(url.replace('{LAT}', lat).replace('{LON}', lon));
-        img.push(url.replace('{LAT}', lat).replace('{LON}', lon));
-        img.push(url.replace('{LAT}', lat).replace('{LON}', lon));
 
-        img.forEach(function(image, index){
-            doc.querySelector('.tile' + (index + 1)).innerHTML = '<img src="' + image + '"/>'
-        });
+        req = new XMLHttpRequest();
+        req.open('GET', 'data/cities.json', true);
+        req.onreadystatechange = function (aEvt) {
+            var city, el;
+            if (req.readyState == 4) {
+                if(req.status == 200) {
+                    cities = JSON.parse(req.responseText);
+                    city = cities.randomElement();
+                    console.log(city);
+                    img.push(url.replace('{LAT}', city.lat).replace('{LON}', city.lon));
+                    city = cities.randomElement();
+                    console.log(city);
+                    img.push(url.replace('{LAT}', city.lat).replace('{LON}', city.lon));
+                    img.forEach(function(image, index){
+                        el = doc.createElement('img');
+                        el.src = image;
+                        el.onload = function(){
+                            this.parentNode.style.display = 'inline-block';
+                        };
+                        doc.querySelector('.tile' + (index + 1)).appendChild(el);
+                    });
+                    // Array.prototype.forEach.call(doc.querySelectorAll('ol li'), function(e){
+                    //     e.style.display = 'inline-block';
+                    // });
+                }
+                else {
+                    doc.querySelector('.error.nodata').style.display = 'block';
+                }
+            }
+        };
+        req.send(null);
     };
 
     locationNotFound = function(){
